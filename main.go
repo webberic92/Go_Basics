6659a6cb538589d29e2df9a2d77bfc995f6ca586
unknown <webberic92@yahoo.com>
2021-08-20 14:51:06 -0400

    lesson7 loops for in range.

--- main.go
+++ main.go
@@ -4,5 +4,23 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("HelloWorld")
+	//Loop
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("Value is :", x)
+	// 	x++
+	// }
+
+	//Loop through array.
+	names := []string{"me", "them", "you", "us"}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	for index, value := range names {
+		fmt.Printf("the values at index %v is %v \n", index, value)
+
+	}
+
 }
