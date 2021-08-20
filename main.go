ba6163cc7c98fc7d7778ae9490f269aab5f51037
unknown <webberic92@yahoo.com>
2021-08-20 11:41:20 -0400

    Go variable basics int, string, floats.

--- main.go
+++ main.go
@@ -4,5 +4,39 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("HelloWorld")
+	//Strings
+
+	//Explicitly typing
+	var nameOne string = "mario"
+	//Infering typing
+	var nameTwo = "luigi"
+	//initialize emtpy string
+	var nameThree string
+	//Short hand for initializing variable.Infers type.
+	//Cant do this OUTSIDE function.
+	nameFour := "yoshi"
+
+	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//Can specify integer size with int8,16,32,64 etc..
+	var numOne int8 = 25
+	var numTwo int8 = -128
+
+	//UINT Can NOT be negative, allows higher then counterpart since no longer accepting minuses.
+	var numThree uint8 = 128
+	fmt.Println(numOne, numTwo, numThree)
+
+	//Floats have decimal points.
+	var scoreOne float32 = 99.99
+	var scoreTwo float64 = 99.999999999
+	//Comes out as 100
+	scoreThree := 99.9999999999999999999999999999999999999999
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
 }
