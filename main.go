11593665c26bf80f52ca9d9cac4285ebaf3951b0
unknown <webberic92@yahoo.com>
2021-08-20 11:55:52 -0400

    Lesson 2 printf println sprintf etc...

--- main.go
+++ main.go
@@ -4,5 +4,27 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("HelloWorld")
+	age := 35
+	name := "ducky"
+
+	//at end of print does not add on a new line.
+	fmt.Print("Hello, ")
+	fmt.Print("World! \n")
+	fmt.Print("newline \n")
+
+	//Println adds new line, plus example of syntax for variables.
+	fmt.Println("Hello again")
+	fmt.Println("goodbye")
+	fmt.Println("my name is ", name, "my age is ", age)
+
+	//PrintF (formatted Strings) %_ is format specifier
+	//%q is quoutes so wont work on integers
+	//%v is variable
+	//%T will tell you type for example it will say float or int.
+	//%f is float and can limit by adding number example %0.1f
+	fmt.Printf("Formated String : my age is %v and my name is %v \n", age, name)
+
+	//SprintF saves formatted strings.
+	var str = fmt.Sprintf("Formated String : my age is %v and my name is %v", age, name)
+	fmt.Println("SAVED STRING :", str)
 }
