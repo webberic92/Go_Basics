121f3a7a8569f9e2d67937d50b61611a1404cdd0
unknown <webberic92@yahoo.com>
2021-08-23 15:13:41 -0400

    working fizzbuzz

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
-	fmt.Println("HelloWorld")
+	fmt.Println("Fizz = 3, Buzz = 5")
+
+	for i := 0; i <= 100; i++ {
+
+		if i%3 == 0 {
+			fmt.Printf("fizz")
+		}
+		if i%5 == 0 {
+			fmt.Printf("buzz")
+		}
+
+		if i%3 != 0 && i%5 != 0 {
+			fmt.Printf(strconv.Itoa(i))
+		}
+		fmt.Println("")
+
+	}
+
+	//Case switch example
+	// for i := 1; i <= 100; i++ {
+	// 	switch {
+	// 	   case i%15==0:
+	// 	   fmt.Println("FizzBuzz")
+	// 	   case i%3==0:
+	// 	   fmt.Println("Fizz")
+	// 	   case i%5==0:
+	// 	   fmt.Println("Buzz")
+	// 	   default:
+	// 	   fmt.Println(i)
+	// 	}
+	//  }
+
 }
