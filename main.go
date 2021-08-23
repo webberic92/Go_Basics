97d414f333d7c4a4368c78174d227d16a75b216f
unknown <webberic92@yahoo.com>
2021-08-23 15:17:24 -0400

    fizz buzz but with final string.

--- main.go
+++ main.go
@@ -10,33 +10,23 @@ func main() {
 	fmt.Println("Fizz = 3, Buzz = 5")
 
 	for i := 0; i <= 100; i++ {
+		finalString := ""
 
 		if i%3 == 0 {
-			fmt.Printf("fizz")
+			finalString = "Fizz"
+
 		}
 		if i%5 == 0 {
-			fmt.Printf("buzz")
+			finalString += "Buzz"
+
 		}
 
 		if i%3 != 0 && i%5 != 0 {
-			fmt.Printf(strconv.Itoa(i))
+			finalString = strconv.Itoa(i)
+
 		}
-		fmt.Println("")
+		fmt.Println(finalString)
 
 	}
-
-	//Case switch example
-	// for i := 1; i <= 100; i++ {
-	// 	switch {
-	// 	   case i%15==0:
-	// 	   fmt.Println("FizzBuzz")
-	// 	   case i%3==0:
-	// 	   fmt.Println("Fizz")
-	// 	   case i%5==0:
-	// 	   fmt.Println("Buzz")
-	// 	   default:
-	// 	   fmt.Println(i)
-	// 	}
-	//  }
 
 }
